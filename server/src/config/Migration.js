15febf42981f0124ed5cb32b08b9dd9d283aff1e
Kyle Ando <ando.kyle44@gmail.com>
2025-08-23 20:52:21 +0800

    changess

--- server/src/config/Migration.js
+++ server/src/config/Migration.js
@@ -11,18 +11,18 @@ const sequelize = db.getSequelizeInstance();
 
 const migrate = async () => {
   try {
-    await sequelize.sync({ alter: true });
-    console.log(`All tables have been created or truncated.`);
-    console.log("Employeer associations:", Object.keys(Employeer.associations));
-    console.log("User associations:", Object.keys(User.associations));
-    // const hashed = await bcrypt.hash("admin123", 10);
-    // await User.create({
-    //   username: "Kyle Ando",
-    //   email: "ando@yahoo.com",
-    //   password: hashed,
-    //   isAdmin: true,
-    // });
-    //await sequelize.close();
+    // await sequelize.sync({ alter: true });
+    // console.log(`All tables have been created or truncated.`);
+    // console.log("Employeer associations:", Object.keys(Employeer.associations));
+    // console.log("User associations:", Object.keys(User.associations));
+    const hashed = await bcrypt.hash("admin123", 10);
+    await User.create({
+      username: "Kyle Ando",
+      email: "ando@yahoo.com",
+      password: hashed,
+      isAdmin: true,
+    });
+    await sequelize.close();
   } catch (error) {
     console.log(error);
   }
